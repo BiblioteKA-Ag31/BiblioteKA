01edc57c53ac3d331fb4f334a40d25d1adf3e475
GedsonCS <gedsoncandido94@gmail.com>
2023-06-30 17:25:52 -0300

    feat:Apploans/ModelLoan/Loanserializer

---
+++ loans/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Loan
+
+
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Loan
+        fields = [
+            "id",
+            "date_exit",
+            "date_devolution",
+            "returned",
+            "quant_pag",
+            "user_id",
+            # "copy_id"
+        ]
+
+        def create(self, validated_data):
+            return Loan.objects.create(**validated_data)
