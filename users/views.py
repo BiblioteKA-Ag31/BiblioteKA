b1db420f101530561464b398e2a6e43581060b67
Danielly Brinquedo <danybrinquedo@hotmail.com>
2023-07-04 11:00:51 -0300

    feat: add userDetailsViews

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 
+from users.permissions import ItsOwnAccount
+
 from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
@@ -7,3 +9,10 @@ from .serializers import UserSerializer
 
 class UserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [ItsOwnAccount]
+
+    queryset = User.objects.all()
