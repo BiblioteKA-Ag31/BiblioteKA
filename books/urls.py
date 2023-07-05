c3d2c555971f781ad0bf70e2a10ddc98038e8d47
GedsonCS <gedsoncandido94@gmail.com>
2023-07-05 09:59:04 -0300

    feat/listBook

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("books/", views.BookView.as_view()),
+    path("books/list/", views.BookListView.as_view()),
     path("books/<int:pk>/", views.BookDetailView.as_view()),
 ]
