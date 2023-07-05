4b1c64abd13ceb5aff46f45e7dcc8879919faf2f
GedsonCS <gedsoncandido94@gmail.com>
2023-07-05 11:27:11 -0300

    feat/permission_loan

--- loans/urls.py
+++ loans/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("loans/", views.LoanView.as_view()),
+    path("loans/list/", views.LoanListView.as_view()),
     path("loans/<int:pk>/", views.LoanDetailView.as_view()),
 ]
