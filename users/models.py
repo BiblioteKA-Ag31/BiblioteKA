7815eaead3e5fa28c2c8be292eec5fce2634223f
Danielly Brinquedo <danybrinquedo@hotmail.com>
2023-07-05 17:22:14 -0300

    feat: model UserBook end send_email

--- users/models.py
+++ users/models.py
@@ -1,13 +1,20 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class UserBook(models.Model):
+    book = models.ForeignKey("books.Book", on_delete=models.CASCADE, related_name="book")
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="user")
+
 class User(AbstractUser):
     email = models.EmailField(max_length=150)
     books = models.ManyToManyField(
-        "books.Book", through="user.UserBook", related_name="users"
+        "books.Book", through="users.UserBook", related_name="users"
     )
 
     @property
     def is_employee(self):
         return self.is_superuser
+
+
