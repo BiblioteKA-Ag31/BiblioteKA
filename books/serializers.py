fbb7953108393960cb94e384be3d87002bdaf5c1
Thiago Almeida <dev.thiagoalmeida@gmail.com>
2023-07-06 12:58:16 -0300

    fix: routes updates / followBook

--- books/serializers.py
+++ books/serializers.py
@@ -5,14 +5,22 @@ from .models import Book, Copy
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ["id","title", "availability", "author", "synopsis", "quant_pag"]
+        fields = [
+            "id",
+            "title",
+            "availability",
+            "author",
+            "synopsis",
+            "quant_pag",
+            "quant_copies",
+        ]
 
         def create(self, validated_data):
             return Book.objects.create(**validated_data)
 
 
 class BookDetailSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True)
+    # books = BookSerializer(many=True)
 
     class Meta:
         model = Book
@@ -23,21 +31,23 @@ class BookDetailSerializer(serializers.ModelSerializer):
             "author",
             "synopsis",
             "quant_pag",
-
+            # "books",
         ]
+
 
 class CopySerializer(serializers.ModelSerializer):
     class Meta:
         model = Copy
         fields = [
             "id",
-            "book_id" ,
+            "book_id",
             "is_available",
         ]
-        
+
         def create(self, validated_data):
+            print(validated_data)
             return Copy.objects.create(**validated_data)
-        
+
         def update(self, instance: Copy, validated_data: dict) -> Copy:
             copy_avalible = validated_data.pop("is_available", None)
 
@@ -47,7 +57,8 @@ class CopySerializer(serializers.ModelSerializer):
             instance.save()
 
             return instance
-        
+
+
 # class CopyDetailsSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Copy
@@ -56,5 +67,3 @@ class CopySerializer(serializers.ModelSerializer):
 #             "book",
 #             "is_available",
 #         ]
-
-
--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,7 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path("users/", views.UserView.as_view()),
-    path("users/book/", views.UserBookView.as_view()),
-    path("users/<int:pk>/", views.UserDetailView.as_view()),
-
+    path("users/<int:pk>/", views.UserDetailsView.as_view()),
+    path("users/books/<int:pk>/", views.UserBookView.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
 ]
