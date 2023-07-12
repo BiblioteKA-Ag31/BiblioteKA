81ded2967ab9bcdaef1cf60da8cdf10d89b7b2e4
Thiago Almeida <dev.thiagoalmeida@gmail.com>
2023-07-12 09:34:55 -0300

    feat: improved copy route and API documentation

--- users/models.py
+++ users/models.py
@@ -1,18 +1,14 @@
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 
 
 class User(AbstractUser):
     email = models.EmailField(max_length=150)
     books = models.ManyToManyField(
-        "books.Book", 
+        "books.Book",
         related_name="users",
     )
 
     @property
     def is_employee(self):
         return self.is_superuser
-
-
